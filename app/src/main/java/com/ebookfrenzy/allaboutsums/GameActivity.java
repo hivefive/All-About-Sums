f90e63a25fa8d9c587cb7c9d29c51035a6039b24
Christoffer Bäckström <christofferb.b@gmail.com>
2015-11-04 03:30:10 +0100

    sumcalc and matching of sums

--- app/src/main/java/com/ebookfrenzy/allaboutsums/GameActivity.java
+++ app/src/main/java/com/ebookfrenzy/allaboutsums/GameActivity.java
@@ -4,13 +4,62 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.EditText;
+import java.util.Random;
+
 
 public class GameActivity extends AppCompatActivity {
+
+    public EditText firstNumberField;
+    public EditText secondNumberField;
+    public int firstNumber;
+    public int secondNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+
+        firstNumber = random();
+
+        firstNumberField = (EditText) findViewById(R.id.numberOne);
+        firstNumberField.setKeyListener(null);
+        firstNumberField.setText("" + firstNumber);
+
+        secondNumber = random();
+
+        secondNumberField = (EditText) findViewById(R.id.numberTwo);
+        secondNumberField.setKeyListener(null);
+        secondNumberField.setText("" + secondNumber);
+    }
+
+    public static int random() {
+        Random generator = new Random();
+
+        int x = generator.nextInt(100);
+        return x;
+    }
+
+    public static int calculateSum(Integer firstNumber, Integer secondNumber) {
+        int sum = firstNumber * secondNumber;
+        return sum;
+    }
+
+    public void matchSums() {
+        EditText sumField = (EditText) findViewById(R.id.sumNumber);
+        int submittedSum = Integer.parseInt(sumField.getText().toString());
+
+        int correctSum = calculateSum(firstNumber, secondNumber);
+
+        TextView resultField = (TextView) findViewById(R.id.gameResult);
+
+        if(submittedSum == correctSum) {
+            resultField.setText("BRAVO");
+        }
+
     }
 
     @Override
